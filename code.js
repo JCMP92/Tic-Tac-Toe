a2e2c886c6b9a6e89f2c95a1abc51b28bcee6416
JCMP92 <arq_juanmp@outlook.com>
2022-03-02 11:14:46 -0600

    Win condition

--- code.js
+++ code.js
@@ -35,18 +35,10 @@ const displayController = (() => {
         cell.addEventListener('click', (e) => {
 
             myGame.gameRound(parseInt(e.target.dataset.index));
-            _updateBoard();
             cell.textContent = gameBoard.boardIndex(cell.id);
 
         }, {once:true});        
     });
-
-    const _updateBoard = () => {
-        for(let i = 0; i<_boardCell; i++){
-            _boardCell[i].textContent = gameBoard.boardIndex(i);
-        }
-    };
-
 })
 ();
 
@@ -54,7 +46,7 @@ const displayController = (() => {
 
 const myGame = (() => {
 
-    const Player = (mark) => {
+    const _Player = (mark) => {
         this.mark = mark;
 
         const playerMark = () => {
@@ -65,19 +57,45 @@ const myGame = (() => {
 
     };
 
-    const playerOne = Player('X');
-    const playerTwo = Player ('O');
+    const playerOne = _Player('X');
+    const playerTwo = _Player ('O');
     let roundCounter = 1;
 
     const gameRound = (boardIndex) => {
 
-        gameBoard.setIndexContent(boardIndex, currentPlayer());
+        gameBoard.setIndexContent(boardIndex, _currentPlayer());
+        if (_whoWins(boardIndex) === true) {
+            window.alert('You win!');
+        }
         roundCounter++;
     }
 
-    const currentPlayer = () => {
+    const _currentPlayer = () => {
         if (roundCounter % 2 === 1) return playerOne.playerMark()
         else return playerTwo.playerMark();
+    }
+
+    const _whoWins = (boardIndex) => {
+        const _winnerComb = [
+            //Horizontal combinations
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            //Vertical combinations
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            //Diagonal combinations
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return _winnerComb
+        .filter((line) => line.includes(boardIndex))
+        .some((winnerLine) => winnerLine.every (
+            (index) => gameBoard.boardIndex(index) === _currentPlayer()
+            )
+        )
     }
     
     return{
